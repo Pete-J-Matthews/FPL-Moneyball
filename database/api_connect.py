c169c878c99f9142c15eb57b85a954e9e9954ff7
Pete Matthews <pete.j.matt@gmail.com>
2023-03-20 23:00:14 +0000

    create user team table
    As the my_team for the current week requires authentication. I've decided to use the previous game week instead so that I can access any user's team just by their user_id. This is enough to make a suggestion as to their team selection.

--- database/api_connect.py
+++ database/api_connect.py
@@ -1,7 +1,6 @@
 import requests
 import mysql.connector
 
-
 # Establish a connection to the MySQL database
 mydb = mysql.connector.connect(
   host="fpldb.c2nundnlfyfy.eu-west-2.rds.amazonaws.com",
@@ -18,6 +17,14 @@ players_url = "https://fantasy.premierleague.com/api/bootstrap-static/"
 players_response = requests.get(players_url)
 players_data = players_response.json()
 
+# Call the FPL fixtures API
+fixtures_url = "https://fantasy.premierleague.com/api/fixtures/?future=1"
+fixtures_response = requests.get(fixtures_url)
+fixtures_data = fixtures_response.json()
+
+# Get the current gameweek
+current_gw = max(fixture['event'] for fixture in fixtures_data)
+
 # Iterate over the player data and insert into MySQL
 for player in players_data['elements']:
     id = player['id']
@@ -27,6 +34,17 @@ for player in players_data['elements']:
     form = player['form']
     value = player['now_cost'] / 10.0
 
+    # Get the next fixture for the player's team
+    next_fixture = next((fixture for fixture in fixtures_data if (fixture['event'] == current_gw) and (fixture['team_h'] == team or fixture['team_a'] == team)), None)
+    if next_fixture:
+        next_fixture_str = f"{next_fixture['team_h']} vs {next_fixture['team_a']}"
+        home_or_away = 'H' if next_fixture['team_h'] == team else 'A'
+        fixture_difficulty = next_fixture['team_h_difficulty'] if home_or_away == 'H' else next_fixture['team_a_difficulty']
+    else:
+        next_fixture_str = "N/A"
+        home_or_away = "N/A"
+        fixture_difficulty = -1
+
     # Check if player already exists in database
     sql = "SELECT id FROM players WHERE id = %s"
     val = (id,)
@@ -35,13 +53,13 @@ for player in players_data['elements']:
     
     if result:
         # Player already exists, update their information
-        sql = "UPDATE players SET name = %s, team = %s, position = %s, form = %s, value = %s WHERE id = %s"
-        val = (name, team, position, form, value, id)
+        sql = "UPDATE players SET name = %s, team = %s, position = %s, form = %s, value = %s, next_fixture = %s, home_or_away = %s, fixture_difficulty = %s WHERE id = %s"
+        val = (name, team, position, form, value, next_fixture_str, home_or_away, fixture_difficulty, id)
         mycursor.execute(sql, val)
     else:
         # Player does not exist, insert new row
-        sql = "INSERT INTO players (id, name, team, position, form, value) VALUES (%s, %s, %s, %s, %s, %s)"
-        val = (id, name, team, position, form, value)
+        sql = "INSERT INTO players (id, name, team, position, form, value, next_fixture, home_or_away, fixture_difficulty) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (id, name, team, position, form, value, next_fixture_str, home_or_away, fixture_difficulty)
         mycursor.execute(sql, val)
 
 mydb.commit()
@@ -67,25 +85,48 @@ for fixture in fixtures_data:
 
 mydb.commit()
 
-# Replace with your email and password
-email = 'pete.j.matt@gmail.com'
-password = 'FPLpassword123!'
+# Get the current gameweek
+current_gw = max(fixture['event'] for fixture in fixtures_data)
 
-session = requests.session()
+# Get the user's team from the previous gameweek
+user_id = 5538943
+previous_gw = current_gw - 1
+picks_url = f"https://fantasy.premierleague.com/api/entry/{user_id}/event/{previous_gw}/picks/"
+picks_response = requests.get(picks_url)
+picks_data = picks_response.json()
 
-# Log in to the Fantasy Premier League website to obtain a session cookie
-login_url = "https://users.premierleague.com/accounts/login/"
-data = {
-    "login": email,
-    "password": password,
-    "app": "plfpl-web",
-    "redirect_uri": "https://fantasy.premierleague.com/",
-}
-with requests.Session() as session:
-    session.post(login_url, data=data, headers={"User-Agent": "Dalvik"})
+# Create a dictionary of players for easy lookup
+players_dict = {player['id']: player for player in players_data['elements']}
 
-    manager_id = 5538943
-    my_team_url = f"https://fantasy.premierleague.com/api/my-team/{manager_id}/"
-    my_team_data = session.get(my_team_url)
-    print(my_team_data.headers)
-    print(my_team_data.text)
+# Clear previous data for this user and gameweek
+mycursor.execute("DELETE FROM user_team WHERE user_id = %s AND gameweek = %s", (user_id, previous_gw))
+
+# Insert the user's team from the previous gameweek into the table
+for pick in picks_data['picks']:
+    player_id = pick['element']
+    player = players_dict[player_id]
+
+    position = pick['position']
+    player_name = f"{player['first_name']} {player['second_name']}"
+    team = player['team']
+    form = player['form']
+
+    # Get the next fixture for the player's team
+    next_fixture = next((fixture for fixture in fixtures_data if (fixture['event'] == current_gw) and (fixture['team_h'] == team or fixture['team_a'] == team)), None)
+    if next_fixture:
+        next_fixture_str = f"{next_fixture['team_h']} vs {next_fixture['team_a']}"
+        home_or_away = 'H' if next_fixture['team_h'] == team else 'A'
+        fixture_difficulty = next_fixture['team_h_difficulty'] if home_or_away == 'H' else next_fixture['team_a_difficulty']
+    else:
+        next_fixture_str = "N/A"
+        home_or_away = "N/A"
+        fixture_difficulty = -1
+
+    captain = pick['is_captain']
+    vice_captain = pick['is_vice_captain']
+
+    sql = """INSERT INTO user_team (user_id, gameweek, player_id, position, player_name, team, form, next_fixture, home_or_away, fixture_difficulty, captain, vice_captain) 
+             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    mycursor.execute(sql, (user_id, previous_gw, player_id, position, player_name, team, form, next_fixture_str, home_or_away, fixture_difficulty, captain, vice_captain))
+
+mydb.commit()
--- database/fpldb.sql
+++ database/fpldb.sql
@@ -9,7 +9,9 @@ CREATE TABLE IF NOT EXISTS players (
   position INT,
   form DECIMAL(4,2),
   value DECIMAL(4,1),
-  selected_by DECIMAL(4,2)
+  next_fixture VARCHAR(255),
+  home_or_away CHAR(1),
+  fixture_difficulty INT
 );
 
 CREATE TABLE IF NOT EXISTS fixtures (
@@ -21,13 +23,30 @@ CREATE TABLE IF NOT EXISTS fixtures (
   away_difficulty INT
 );
 
--- Table for my team players
-CREATE TABLE IF NOT EXISTS my_team (
-  id INT PRIMARY KEY,
-  name VARCHAR(255),
-  team INT,
-  position INT,
-  form FLOAT,
-  value FLOAT,
-  is_captain BOOLEAN
+-- Table for user team
+CREATE TABLE IF NOT EXISTS user_team (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    gameweek INT NOT NULL,
+    player_id INT NOT NULL,
+    position INT NOT NULL,
+    player_name VARCHAR(255) NOT NULL,
+    team INT NOT NULL,
+    form DECIMAL(4,1) NOT NULL,
+    value DECIMAL(4,1) NOT NULL,
+    next_fixture VARCHAR(255) NOT NULL,
+    home_or_away CHAR(1) NOT NULL,
+    fixture_difficulty INT NOT NULL,
+    captain BOOLEAN NOT NULL,
+    vice_captain BOOLEAN NOT NULL
 );
+
+-- Sort the players table by fixture difficulty ascending, home/away descending, form descending, and value descending
+SELECT *
+FROM players
+ORDER BY fixture_difficulty ASC, home_or_away DESC, form DESC, value DESC;
+
+-- Sort the user_team table by fixture difficulty ascending, home/away descending, form descending, and value descending
+SELECT *
+FROM user_team
+ORDER BY fixture_difficulty ASC, home_or_away DESC, form DESC, value DESC;
