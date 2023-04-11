3d8ef29f8d4e08288eae9eec5f70400445d2797d
Pete Matthews <pete.j.matt@gmail.com>
2023-04-11 22:36:40 +0100

    iterate each page of API

--- database/users.py
+++ database/users.py
@@ -1,5 +1,5 @@
-import requests
 import mysql.connector
+import requests
 
 # Establish a connection to the MySQL database
 mydb = mysql.connector.connect(
@@ -17,19 +17,29 @@ def insert_user_data(user_id, username, team_name, rank):
     cursor.execute(query, values)
     mydb.commit()
 
-# Fetch data from the FPL API
-url = "https://fantasy.premierleague.com/api/leagues-classic/314/standings/"
-response = requests.get(url)
-data = response.json()
+# Function to fetch data from the FPL API with pagination
+def fetch_data(page_number):
+    url = f"https://fantasy.premierleague.com/api/leagues-classic/314/standings/?page={page_number}"
+    response = requests.get(url)
+    return response.json()
 
 # Parse and insert data into the "users" table
-for entry in data['standings']['results']:
-    user_id = entry['entry']
-    username = entry['player_name']
-    team_name = entry['entry_name']
-    rank = entry['rank']
+page = 1
+has_next = True
 
-    insert_user_data(user_id, username, team_name, rank)
+while has_next:
+    data = fetch_data(page)
+    has_next = data['standings']['has_next']
+
+    for entry in data['standings']['results']:
+        user_id = entry['entry']
+        username = entry['player_name']
+        team_name = entry['entry_name']
+        rank = entry['rank']
+
+        insert_user_data(user_id, username, team_name, rank)
+
+    page += 1
 
 # Close the database connection
 mydb.close()
